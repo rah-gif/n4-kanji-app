cc39f70f6aee568425b66beeb6f425f5b14f3a8a
Chethana Rahul <rahulchethana65@gmail.com>
2026-02-20 18:00:43 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // [https://vitejs.dev/config/](https://vitejs.dev/config/)
 export default defineConfig({
   plugins: [react()],
-  base: "/n4-kanji-app/", // <--- IMPORTANT: Must match your GitHub Repo name
-})
+  base: "/n4-kanji-app/", 
